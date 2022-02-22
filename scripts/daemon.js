bb70d1364f20eba87c8513fab66768fe374f1f49
Paul Kirth <paulkirth@google.com>
2022-02-21 18:19:55 -0800

    Overhaul daemon.js

--- scripts/daemon.js
+++ scripts/daemon.js
@@ -1,206 +1,222 @@
 import { breakPorts, can_root } from "/scripts/utils.js";
 
-
 let id = 0;
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    // initialization & config
-    // set up any constants
-    const host = ns.getHostname();
+  // initialization & config
+  // set up any constants
+  const host = ns.getHostname();
 
-    const loop_period = 10000; //millisec
+  const loop_period = 200; //millisec
 
-    const hack_script = "/scripts/basics/hack.js";
-    const grow_script = "/scripts/basics/grow.js";
-    const weaken_script = "/scripts/basics/weaken.js";
+  const hack_script = "/scripts/basics/hack.js";
+  const grow_script = "/scripts/basics/grow.js";
+  const weaken_script = "/scripts/basics/weaken.js";
 
-    // calculate static values
+  ns.disableLog("getServerMaxRam");
+  ns.disableLog("getServerUsedRam");
+  //ns.disableLog("exec");
+  // calculate static values
 
-    // create necessary data structures
+  // create necessary data structures
 
-    // check for the precense of our data files
+  // check for the precense of our data files
 
-    var preping = [];
-    var hacks = {};
+  var preping = [];
+  var hacks = {};
 
-    let debug = false;
-    let loop = true;
-    let initialized = false;
-    ns.print("Begining loop");
-    // active loop
-    do {
-        // once per period scan servers and write data to file
-        // collect a variety of data and parameter about the server
-        // if servers are too poor or sec too high, our scripts are not working correctly, kill and add to prep list
-        ns.exec("/scripts/collect_servers.js", host);
-        await ns.sleep(100);
-        var servers = JSON.parse(ns.read("/data/server.json.txt"));
-        if (!initialized) {
-            for (let s of servers) {
-                if (s.name === "home")
-                    continue;
-                //ns.print("killing processes on " + s.name);
-                ns.killall(s.name);
-            }
-            initialized = true;
-            ns.print("initialized");
+  let debug = false;
+  let loop = true;
+  let initialized = false;
+  ns.print("Begining loop");
+  // active loop
+  do {
+    // once per period scan servers and write data to file
+    // collect a variety of data and parameter about the server
+    // if servers are too poor or sec too high, our scripts are
+    // not working correctly, kill and add to prep list
+    ns.exec("/scripts/collect_servers.js", host);
+    await ns.sleep(100);
+    var servers = JSON.parse(ns.read("/data/server.json.txt"));
+    if (!initialized) {
+      for (let s of servers) {
+        if (s.name === "home") continue;
+        //ns.print("killing processes on " + s.name);
+        await ns.killall(s.name);
+      }
+      initialized = true;
+      ns.print("initialized");
+    }
+    let to_prepare = [];
+    let workers = [];
+    let targets = [];
+    let future = [];
+
+    // categorize servers into groups
+    for (let s of servers) {
+      // targets we can't root go into a future list
+      // TODO: consider if we should just ignore alltogether
+      if (!s.is_rooted) {
+        future.push(s);
+        continue;
+      }
+
+      // targets we can't hack yet <-- can be extra compute if rooted
+      // targets that are not profitable to hack <-- i.e, they can be extra compute
+      //if (s.max_money == 0 || !s.can_hack || s.name == "home") {
+      if (s.max_money == 0 || s.name == "home") {
+        workers.push(s);
+        continue;
+      }
+
+      // Targets we want to hack, but need to prepare
+      if (s.cur_money < 0.8 * s.max_money || s.cur_sec > s.min_sec + 1) {
+        to_prepare.push(s);
+        continue;
+      }
+
+      // Everything else is a target we can hack now, unless wer're already hacking it
+      let t = hacks[s.name];
+      if (t != undefined && t.length != 0) {
+        if (t.some(ns.isRunning)) {
+          workers.push(s);
+          continue;
         }
-        let to_prepare = [];
-        let workers = [];
-        let targets = [];
-        let future = [];
+        delete hacks[s.name];
+        //ns.tprint(s.name, Object.keys(hacks));
+      }
+      targets.push(s);
+    } // end for loop
 
-        // categorize servers into groups
-        for (let s of servers) {
-            // targets we can't root go into a future list 
-            // TODO: consider if we should just ignore alltogether
-            if (!s.is_rooted) {
-                future.push(s);
-                continue;
-            }
+    if (debug) {
+      ns.tprint("Future: ");
+      ns.tprint(future);
+      ns.tprint("Targets ");
+      ns.tprint(targets);
+      ns.tprintf("Workers: %d", workers.length);
+      ns.tprintf("Perpare: %d", to_prepare.length);
+      ns.tprint(to_prepare);
+      ns.tprintf("Total: %d", servers.length);
+    }
 
-            // targets we can't hack yet <-- can be extra compute if rooted
-            // targets that are not profitable to hack <-- i.e, they can be extra compute
-            if (s.max_money == 0 || !s.can_hack || s.hack_chance < 0.4 || s.name == "home") {
-                workers.push(s);
-                continue;
-            }
+    //sort workers by name, then by ram
+    workers.sort((a, b) => a.name - b.name);
+    workers.sort((a, b) => getRam(ns, b) - getRam(ns, a));
+    ns.print(workers.map((a) => a.name));
 
-            // Targets we want to hack, but need to prepare
-            if (s.cur_money < 0.9 * s.max_money) {
-                to_prepare.push(s);
-                continue;
-            }
+    // clean out prepping list
+    preping = preping.filter((a) => a.pids.some(ns.isRunning));
+    to_prepare.sort((a, b) => a.max_money - b.max_money);
 
-            // Everything else is a target we can hack now
-            let t = hacks[s.name];
-            if (t != undefined && t.length != 0) {
-                for (let pid of t) {
-                    if (ns.isRunning(pid)) {
-                        ns.print(ns.vsprintf("Already hacking %s", s.name));
-                        workers.push(s)
-                        continue;
-                    }
-                }
-                delete hacks[s.name];
-            }
-            targets.push(s);
-        }
+    //TODO: figure out when prep is over and schedule batching to begin ASAP
+    // for ever target we need to prepare, devote resources to prep
+    ns.print("Begin Prepare phase for " + to_prepare.length + " servers");
+    ns.print(to_prepare.map((a) => a.name));
+    for (let p of to_prepare) {
+      await prepare(ns, preping, hacks, workers, p);
+      //let x = prepare(ns, preping, workers, p);
+      //Promise.all;
+    }
 
-        if (debug) {
-            ns.tprint("Future: ");
-            ns.tprint(future);
-            ns.tprint("Targets ");
-            ns.tprint(targets);
-            ns.tprintf("Workers: %d", workers.length);
-            ns.tprintf("Perpare: %d", to_prepare.length);
-            ns.tprint(to_prepare);
-            ns.tprintf("Total: %d", servers.length);
-        }
+    ns.print(
+      "Begin Hacking phase for " + targets.length + " new servers, already hacking " + Object.keys(hacks).length,
+    );
+    targets.sort((a, b) => a.max_money - b.max_money);
+    // hack prepared targets
+    for (let t of targets) {
+      let s_g = t.weaken_time - t.grow_time + 50;
+      let s_h = t.weaken_time - t.hack_time + 150;
+      let batches = Math.ceil(Math.min(/*once per sec*/ t.weaken_time / 1000, 100));
 
-        workers.sort((a, b) => a.name - b.name);
-        workers.sort((a, b) => b.ram - a.ram);
-        ns.print(workers.map(a => a.name));
-        to_prepare.sort((a, b) => a.max_money - b.max_money);
-        //TODO: figure out when prep is over and schedule batching to begin ASAP
-        // for ever target we need to prepare, devote resources to prep
-        for (let p of to_prepare) {
-            await prepare(ns, preping, workers, p);
-        }
+      let period = t.weaken_time / batches;
 
-        targets.sort((a, b) => a.max_money - b.max_money);
-        // hack prepared targets
-        for (let t of targets) {
+      for (let i = 0; i < batches; i++) {
+        let pid = await run_wgh(ns, workers, t.name, t.t_h, s_h + i * period, true, hack_script, id);
+        let x = hacks[t.name];
+        if (x != undefined) hacks[t.name].push(pid);
+        else hacks[t.name] = [pid];
 
-            let s_g = t.weaken_time - t.grow_time + 50;
-            let s_h = t.weaken_time - t.hack_time + 150;
-            let batches = Math.min(/*once per sec*/ t.weaken_time / 1000, 100);
+        hacks[t.name].push(await run_wgh(ns, workers, t.name, t.t_w_h, i * period, true, weaken_script, id));
+        hacks[t.name].push(await run_wgh(ns, workers, t.name, t.t_g, s_g + i * period, true, grow_script, id));
+        hacks[t.name].push(await run_wgh(ns, workers, t.name, t.t_w_g, 100 + i * period, true, weaken_script, id++));
+      }
+    }
+    ns.print("End Hacking phase for " + targets.length + " new servers, already hacking " + Object.keys(hacks).length);
 
-            let period = t.weaken_time / batches;
+    ns.print("Begin Rooting phase for " + future.length + " servers");
+    for (let f of future) {
+      if (await can_root(ns, f.name)) {
+        breakPorts(ns, f.name);
+        ns.nuke(f.name);
+      }
+    }
 
-            for (let i = 0; i < batches; i++) {
-
-                let pid = await run_wgh(ns, workers, t.name, t.t_h, s_h + (i * period), true, hack_script, id);
-                let x = hacks[t.name];
-                if (x != undefined)
-                    hacks[t.name].push(pid);
-                else
-                    hacks[t.name] = [pid];
-
-                await run_wgh(ns, workers, t.name, t.t_w, i * period, true, weaken_script, id);
-                await run_wgh(ns, workers, t.name, t.t_g, s_g + (i * period), true, grow_script, id);
-                await run_wgh(ns, workers, t.name, t.t_w_g, 100 + (i * period), true, weaken_script, id++);
-            }
-        }
-
-        for (let f of future) {
-            if (await can_root(ns, f.name)) {
-                breakPorts(ns, f.name);
-                ns.nuke(f.name);
-            }
-        }
-
-        await ns.sleep(loop_period);
-    } while (loop);
-    ns.print("Somehow exiting daemon!");
+    await ns.sleep(loop_period);
+  } while (loop);
+  ns.print("Somehow exiting daemon!");
 }
 
 /** @param {NS} ns **/
-async function prepare(ns, preping, workers, target) {
-    // don't prepare something already being prepared
-    let t = preping.find(a => a.host == target.name);
-    if (t != undefined) {
-        for (let pid of t.pids) {
-            if (ns.isRunning(pid)) {
-                ns.print(ns.vsprintf("Already preping %s", target.name));
-                return;
-            }
-        }
-        preping = preping.filter(a => a != t);
+async function prepare(ns, preping, hacks, workers, target) {
+  // don't prepare something already being prepared
+  if (preping.some((a) => a.host == target.name)) return;
+
+  // stop any running hacks if we want to prep this thing
+  if (hacks[target.name] != undefined) {
+    //ns.tprint("Killing to prep on " + target.name);
+    for (let pid of hacks[target.name]) {
+      ns.kill(pid);
     }
+  }
 
-    let tg = target.t_g_i;
-    let twi = target.t_w_g_i;
-    let twg = target.t_w_i;
-    let grow_time = target.grow_time;
-    let weak_time = target.weaken_time;
+  let tg = target.t_g_i;
+  let twi = target.t_w_i;
+  let twg = target.t_w_g_i;
+  let grow_time = target.grow_time;
+  let weak_time = target.weaken_time;
 
-    let s_g = weak_time - grow_time + 50;
-    const grow_script = "/scripts/basics/grow.js";
-    const weaken_script = "/scripts/basics/weaken.js";
+  let s_g = weak_time - grow_time + 50;
+  const grow_script = "/scripts/basics/grow.js";
+  const weaken_script = "/scripts/basics/weaken.js";
 
+  let wi = await run_wgh(ns, workers, target.name, twi, 0, false, weaken_script, id++);
+  let gi = await run_wgh(ns, workers, target.name, tg, s_g, false, grow_script, id++);
+  let wg = await run_wgh(ns, workers, target.name, twg, 100, false, weaken_script, id++);
 
-    let wi = await run_wgh(ns, workers, target.name, twi, 0, false, weaken_script, id++);
-    let gi = await run_wgh(ns, workers, target.name, tg, s_g, false, grow_script, id++);
-    //TODO: remove this debug block, when we're sure it is no longer needed
-    if (twg < 1) {
-        ns.tprint("Error on:");
-        ns.tprint(target);
-    }
-    let wg = await run_wgh(ns, workers, target.name, twg, 100, false, weaken_script, id++);
-
-    preping.push({ "host": target.name, "pids": [wi, gi, wg] });
-
+  preping.push({ host: target.name, pids: [wi, gi, wg] });
 }
 
-
-/** @param {NS} ns **/
+/**
+ * @param {import(".").NS } ns
+ * @param {[any] }workers
+ * @param {string} target
+ * @param {number} required_threads
+ * @param {number} sleep_time
+ * @param {bool} loop
+ * @param {string} script
+ * @param {number} id
+ */
 async function run_wgh(ns, workers, target, required_threads, sleep_time, loop, script, id) {
-    //ns.tprint(workers);
-    //workers.sort((a,b)=> getRam(b) - getRam(a));
-    // find server to run script w/ required threads
-    for (let w of workers) {
-        let ram = ns.getServerMaxRam(w.name) - ns.getServerUsedRam(w.name);
-        let t = ram / ns.getScriptRam(script);
-        if (t < required_threads)
-            continue;
-        let l = loop ? "-l" : "";
-        return ns.exec(script, w.name, required_threads, target, sleep_time, l, id);
+  // TODO: make this schedule whatever threads are able, and then move onto the next server
+  // i.e, schedule a partial task on s1, then the rest on s2, etc.
+
+  // find server to run script w/ required threads
+  for (let w of workers) {
+    let ram = ns.getServerMaxRam(w.name) - ns.getServerUsedRam(w.name);
+    let t = Math.floor(ram / ns.getScriptRam(script, w.name));
+    let l = loop ? "-l" : "";
+    if (t < required_threads) {
+      continue;
     }
-    return 0;
+    return ns.exec(script, w.name, required_threads, target, sleep_time, l, id);
+  }
+  //TODO: make this a log message
+  ns.tprintf("No Server had enough ram to run %s: %.2fGB", script, ns.getScriptRam(script));
+  return 0;
 }
 
+/**  @param {import(".").NS } ns **/
 function getRam(ns, host) {
-    return host.ram - ns.getServerUsedRam(host.name);
+  return host.ram - ns.getServerUsedRam(host.name);
 }
