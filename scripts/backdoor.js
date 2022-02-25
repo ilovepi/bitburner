01ce979f7b6adfc9bfe2e8c4469928dbc80c3eaf
Paul Kirth <paulkirth@google.com>
2022-02-24 23:06:42 -0800

    Clean up backdoor script

--- scripts/backdoor.js
+++ scripts/backdoor.js
@@ -1,15 +1,9 @@
 import { find_all_servers } from "/scripts/utils.js";
 
-/** @param {NS} ns **/
+/** @param {import(".").NS } ns */
 export async function main(ns) {
-    let servers = await find_all_servers(ns);
-    for(let s of servers)
-    {
-        ns.installBackdoor(s);
-
-    }
-
-
-    
-
+  let servers = await find_all_servers(ns);
+  for (let s of servers) {
+    ns.installBackdoor(s);
+  }
 }
