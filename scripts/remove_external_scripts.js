f3204a7156f26e98245803655d05a8f8fb870ac6
Paul Kirth <pk1574@gmail.com>
2023-04-02 21:49:39 -0700

    Fix typo in removal script

--- scripts/remove_external_scripts.js
+++ scripts/remove_external_scripts.js
@@ -4,8 +4,8 @@ export async function main(ns) {
   let servers = await find_all_servers(ns);
   for (let s of servers) {
     if (s == "home") continue;
-    ns.rm("/script/basics/hack.js", s);
-    ns.rm("/script/basics/grow.js", s);
-    ns.rm("/script/basics/weaken.js", s);
+    ns.rm("/scripts/basics/hack.js", s);
+    ns.rm("/scripts/basics/grow.js", s);
+    ns.rm("/scripts/basics/weaken.js", s);
   }
 }
