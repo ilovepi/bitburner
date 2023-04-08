a878f91f448f78c9609b26139413eb2cc46a5d07
Paul Kirth <pk1574@gmail.com>
2023-04-08 12:49:07 -0700

    Add hack and weaken prototypes

---
+++ scripts/basics/new_hack.js
@@ -0,0 +1,17 @@
+/** @param {import("..").NS } ns **/
+export async function main(ns) {
+  // hack.js [target] [delay] [sleep] [-l] [id]
+  let data = ns.flags([
+    ["target", "n00dles"],
+    ["delay", 0],
+    ["sleep", 0],
+    ["loop", false],
+    ["stocks", false],
+    ["id", 0],
+  ]);
+  await ns.sleep(data["delay"]);
+  do {
+    await ns.sleep(data["sleep"]);
+    await ns.hack(data["target"], {stock: data["stocks"]});
+  } while (data["loop"]);
+}
