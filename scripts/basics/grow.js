b39de5c2176a1b1f7cd203029b46091a3b093d70
Paul Kirth <pk1574@gmail.com>
2023-04-08 12:37:57 -0700

    Don't affect stock market with grow and hack scripts

--- scripts/basics/grow.js
+++ scripts/basics/grow.js
@@ -4,6 +4,6 @@ export async function main(ns) {
   await ns.sleep(ns.args[1]);
   do {
     await ns.sleep(ns.args[2]);
-    await ns.grow(ns.args[0]);
+    await ns.grow(ns.args[0], {stock: false});
   } while (ns.args[3] == "-l");
 }
--- scripts/basics/hack.js
+++ scripts/basics/hack.js
@@ -1,9 +1,11 @@
-/** @param {NS} ns **/
+/**
+ * @param {import("..").NS } ns
+ **/
 export async function main(ns) {
   // hack.js [target] [delay] [sleep] [-l] [id]
   await ns.sleep(ns.args[1]);
   do {
     await ns.sleep(ns.args[2]);
-    await ns.hack(ns.args[0]);
+    await ns.hack(ns.args[0], {stock: false});
   } while (ns.args[3] == "-l");
 }
