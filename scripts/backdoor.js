b5e4e7eb6590caf19a9264ae858f3b682b9b348f
Paul Kirth <pk1574@gmail.com>
2023-04-08 12:33:31 -0700

    Rewrite backdoor script after ns.installBackdoor() API changes

--- scripts/backdoor.js
+++ scripts/backdoor.js
@@ -1,9 +1,83 @@
-import { find_all_servers } from "/scripts/utils.js";
+import { can_hack } from "/scripts/utils.js";
 
-/** @param {import(".").NS } ns */
+const max_depth = 100;
+
+/**
+ * @param {import(".").NS } ns
+ */
 export async function main(ns) {
-  let servers = await find_all_servers(ns);
-  for (let s of servers) {
-    ns.installBackdoor(s);
+  let visited = [];
+  let path = [];
+  let path_map = new Map();
+  await DFS(ns, ns.getHostname(), visited, path, path_map, max_depth);
+
+  for (const [host, path] of path_map.entries()) {
+    if (ns.getServer(host).backdoorInstalled) continue;
+    if (await can_hack(ns, host)) {
+      // connect to target
+      connect(ns, path);
+      await ns.singularity.installBackdoor();
+      // come back to where we started
+      connect(ns, path.slice().reverse());
+    }
   }
+}
+
+/**
+ * @param {import(".").NS } ns
+ * @param {[string]} path
+ */
+function connect(ns, path) {
+  for (let p of path) {
+    ns.singularity.connect(p);
+  }
+}
+
+/**
+ * @param {import(".").NS } ns
+ * @param {string} target
+ * @param {[string]} visited
+ * @param {[string]} worklist
+ * @param {Map<string,[string]>} path
+ */
+async function visit(ns, target, visited, worklist, path) {
+  if (!ns.getServer(host).backdoorInstalled && (await can_hack(ns, target))) {
+    await ns.singularity.installBackdoor();
+  }
+  visited.push(target);
+  path[target].push(target);
+
+  let adjacent = ns.scan(target);
+  for (let a of adjacent) {
+    if (visited.includes(a)) {
+      continue;
+    }
+    worklist.push(a);
+  }
+}
+
+/**
+ * @param {import(".").NS } ns
+ * @param {string} target
+ * @param {[string]} visited
+ * @param {[string]} worklist
+ * @param {[string]} path
+ * @param {Map<string,[string]>} path_map
+ */
+async function DFS(ns, node, visited, path, path_map, depth) {
+  if (depth == 0) {
+    ns.tprint("Error: Hit max depth limit!");
+    exit(-1);
+  }
+  visited.push(node);
+  path.push(node);
+  path_map.set(node, [...path]);
+  let adjacent = ns.scan(node);
+  for (let a of adjacent) {
+    if (visited.includes(a)) {
+      continue;
+    }
+    await DFS(ns, a, visited, path, path_map, depth - 1);
+  }
+  path.pop(1);
 }
